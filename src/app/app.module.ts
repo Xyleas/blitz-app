0797ac4609db451b512ce939785d7573123bea90
Danny Riggleman <dannyjriggleman09@gmail.com>
2022-01-25 15:07:07 -0500

    Lab 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FooterComponent } from 'src/footer/footer.component';
+import { HeaderComponent } from 'src/header/header.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
