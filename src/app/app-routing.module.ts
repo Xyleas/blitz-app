56d0877b7fbdf25c8558bf348a70fa4791eb81c6
Danny Riggleman <dannyjriggleman09@gmail.com>
2022-02-15 14:54:35 -0500

    Continued Angular Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LeagueComponent } from 'src/league/league.component';
 import { ValorantComponent } from 'src/valorant/valorant.component';
+import { LegendsComponent } from 'src/legends/legends.component';
+import { FortniteComponent } from 'src/fortnite/fortnite.component';
+import { TeamfightTacticesComponent } from 'src/tactics/tactics.component';
 
 const routes: Routes = [
   { path: 'http://localhost:4200/league', component: LeagueComponent },
-  { path: 'http://localhost:4200/valorant', component: ValorantComponent }
+  { path: 'http://localhost:4200/valorant', component: ValorantComponent },
+  { path: 'http://localhost:4200/legends', component: LegendsComponent},
+  { path: 'http://localhost:4200/fortnite', component: FortniteComponent},
+  { path: 'http://localhost:4200/tactics', component: TeamfightTacticesComponent},
+
 ];
 
 
